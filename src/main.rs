e431e0051f79b2e8e316c72daf8182364d021123
hannah <hannah.ellis@pulsic.com>
2019-07-20 22:46:15 +0100

    branch function

--- src/main.rs
+++ src/main.rs
@@ -122,6 +122,65 @@ fn solved_config() -> Configuration {
     ]
 }
 
+fn solved_config_ref<'t>() -> ConfigurationRef<'t> {
+    [
+        &Value::White,  //0
+        &Value::White,  //1
+        &Value::White,  //2
+        &Value::White,  //3
+        &Value::White,  //4
+        &Value::White,  //5
+        &Value::White,  //6
+        &Value::White,  //7
+        &Value::White,  //8
+        &Value::Orange, //9
+        &Value::Orange, //10
+        &Value::Orange, //11
+        &Value::Green,  //12
+        &Value::Green,  //13
+        &Value::Green,  //14
+        &Value::Red,    //15
+        &Value::Red,    //16
+        &Value::Red,    //17
+        &Value::Orange, //18
+        &Value::Orange, //19
+        &Value::Orange, //20
+        &Value::Green,  //21
+        &Value::Green,  //22
+        &Value::Green,  //23
+        &Value::Red,    //24
+        &Value::Red,    //25
+        &Value::Red,    //26
+        &Value::Orange, //27
+        &Value::Orange, //28
+        &Value::Orange, //29
+        &Value::Green,  //30
+        &Value::Green,  //31
+        &Value::Green,  //32
+        &Value::Red,    //33
+        &Value::Red,    //34
+        &Value::Red,    //35
+        &Value::Blue,   //36
+        &Value::Blue,   //37
+        &Value::Blue,   //38
+        &Value::Blue,   //39
+        &Value::Blue,   //40
+        &Value::Blue,   //41
+        &Value::Blue,   //42
+        &Value::Blue,   //43
+        &Value::Blue,   //44
+        &Value::White,  //45
+        &Value::Yellow, //46
+        &Value::Yellow, //47
+        &Value::Yellow, //48
+        &Value::Yellow, //49
+        &Value::Yellow, //50
+        &Value::Yellow, //51
+        &Value::Yellow, //52
+        &Value::Yellow, //53
+    ]
+}
+
 fn realise(c: &ConfigurationRef) -> Configuration {
     let mut o = [Value::White; 54];
     (0..54).for_each(|index| o[index] = *(c[index]));
@@ -286,6 +345,14 @@ impl fmt::Debug for Operation {
     }
 }
 
+impl Copy for Operation {}
+
+impl Clone for Operation {
+    fn clone(&self) -> Operation {
+        *self
+    }
+}
+
 fn operation_to_function<'t>(
     x: &'t Operation,
 ) -> Box<fn(&'t ConfigurationRef) -> ConfigurationRef<'t>> {
@@ -372,13 +439,13 @@ mod list {
     }
 }
 
-struct Step {
+struct Step<'t> {
     last: Operation,
-    config: Configuration,
+    config: ConfigurationRef<'t>,
 }
 
-impl Step {
-    fn new(c:Configuration) -> Step {
+impl<'t> Step<'t> {
+    fn new(c: ConfigurationRef<'t>) -> Step {
         Step {
             last: Operation::Identity,
             config: c,
@@ -386,10 +453,42 @@ impl Step {
     }
 
     fn solved(&self) -> bool {
-        let master = solved_config();
+        let master = solved_config_ref();
         (0..54)
             .map(|index| self.config[index] == master[index])
             .fold(true, |acc, x| acc && x)
+    }
+
+    fn branch(&self) -> Vec<Step> {
+        let _xs = [
+            Operation::TopRight,
+            Operation::TopLeft,
+            Operation::FrontRight,
+            Operation::FrontLeft,
+            Operation::RightFront,
+            Operation::RightBack,
+            Operation::BottomRight,
+            Operation::BottomLeft,
+            Operation::BackRight,
+            Operation::BackLeft,
+            Operation::LeftFront,
+            Operation::LeftBack,
+        ]
+        .iter()
+        .map(|op| {
+            let config = operation_to_function(op)(&self.config);
+            Step {
+                last: *op,
+                config: config,
+            }
+        });
+        /*.map(|(op, func)| (op, realise(&func(&to_ref(&self.config)))))
+        .map(|(op, newConfig)| Step {
+            last: *op.clone(),
+            config: newConfig,
+        })
+        .collect::<Vec<_>>()*/
+        Vec::new()
     }
 }
 
